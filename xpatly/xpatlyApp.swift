921570e7a846afa430d27f8c4c665060212b13de
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2023-01-14 18:23:17 +0100

    Initial Commit

---
+++ xpatly/xpatlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  xpatlyApp.swift
+//  xpatly
+//
+//  Created by Sebastian Gustavsson on 2023-01-14.
+//
+
+import SwiftUI
+
+@main
+struct xpatlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
